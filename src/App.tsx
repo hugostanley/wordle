f61eac390023dd9cecab087199724e84deeff153
hugostanley <stanleyhugo06@gmail.com>
2023-05-19 23:56:01 +0800

    feat(): add input capturing logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+
+type Entry = string[]
+type EntriesCollecton = Array<Entry>
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentEntryState, setCurrentEntryState] = useState<Entry>([])
+  const [entriesCollectionState, setEntriesCollectionState] = useState<EntriesCollecton>([])
+  const [enterPressed, setEnterPressed] = useState<boolean>(false)
+
+  function setKeyboardInputToEntry(e: KeyboardEvent) {
+    const key = e.key;
+    /** Check if input is only from letters a-z */
+    const isAlphabetical = /^[a-zA-Z]+$/.test(key)
+
+    /** Push input letter to entry state array */
+    setCurrentEntryState(state => {
+      setEnterPressed(false)
+      if (state.length < 6 && isAlphabetical && key.length === 1) {
+        return [...state, e.key]
+      } else {
+        return state
+      }
+    })
+
+    if (e.key === 'Enter') {
+      setEnterPressed(true)
+    }
+  }
+
+
+  useEffect(() => {
+    if (enterPressed === true) {
+      setEntriesCollectionState(state => [...state, currentEntryState])
+      setCurrentEntryState([])
+    }
+  }, [enterPressed])
+
+  /* Listen for keyboard types within the window object. 
+   *
+   * Make sure to remove the event listener throught the useEffect cleanup function 
+   * to avoid double entries
+   *
+   * */
+  useEffect(() => {
+    window.addEventListener('keydown', setKeyboardInputToEntry)
+    return () => {
+      window.removeEventListener('keydown', setKeyboardInputToEntry)
+    }
+  }, [])
 
   return (
-    <>
+    <div>
+      {entriesCollectionState.map((arr, idx) => {
+        return (
+          <>
+            <div key={idx}>
+              {
+                arr.map(str => {
+                  return str
+                })}
+            </div>
+          </>
+        )
+      })}
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {currentEntryState}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
